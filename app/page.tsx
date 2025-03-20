eda994f60a5a46ecb331e5ca9bb3fe310cd7a5f1
Niklas P <contact@niklasplessing.net>
2025-03-20 23:40:26 +0100

    deploy button

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,13 @@ export default async function Home() {
         Polkadot Next.js Starter
       </h1>
       <p>A starter project for building a Polkadot dApp with Next.js.</p>
+      <a
+        className="hidden sm:block"
+        href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fniklasp%2Fpolkadot-nextjs-starter"
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+      </a>
     </main>
   );
 }
